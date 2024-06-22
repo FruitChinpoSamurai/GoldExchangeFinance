ef3f52bc36ea6cf5dbc29e1d0d1372ea9e3a0dd4
AyyRmao <animerjk@gmail.com>
2024-06-22 18:39:53 +0500

    reeeee

--- server/database.js
+++ server/database.js
@@ -2,8 +2,8 @@ const Pool = require("pg").Pool;
 
 const pool = new Pool({
     user: "postgres",
-    // password: "allisgood",
-    password: "goldexchangeman",
+    password: "allisgood",
+    // password: "goldexchangeman",
     port: 5432,
     database: "goldbuysell"
 });
